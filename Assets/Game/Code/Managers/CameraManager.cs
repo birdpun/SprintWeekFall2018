dd161df580b33bd40e1a033af40cb6b7d5bef33c
Del <del.yn@hotmail.com>
2018-11-01 17:38:38 -0400

    update

---
+++ Assets/Game/Code/Managers/CameraManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Blur = SuperBlur.SuperBlur;
+
+[ExecuteInEditMode]
+public class CameraManager : MonoBehaviour
+{
+    private static CameraManager instance;
+
+    public float fov = 15f;
+    public float shakeSettle = 10f;
+
+    [ClampedCurve]
+    public AnimationCurve startingPlayingCurve;
+
+    [ClampedCurve]
+    public AnimationCurve focusOnWinner;
+
+    private Blur blur;
+    private float shake;
+    private Camera cam;
+    private float startingTime;
+    private float celebratingTime;
+    private GameState lastState;
+
+    public static Transform Transform
+    {
+        get
+        {
+            if (!instance) instance = FindObjectOfType<CameraManager>();
+
+            return instance.cam.transform;
+        }
+    }
+
+    private void Awake()
+    {
+        cam = FindObjectOfType<Camera>();
+        blur = cam.GetComponent<Blur>();
+    }
+
+    private void OnEnable()
+    {
+        instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        cam.fieldOfView = fov;
+
+        if (!Application.isPlaying) return;
+
+        Vector3 waitingPosition = new Vector3(0f, 60f, -140f);
+        Vector3 playingPosition = new Vector3(0f, 60f, -70f);
+        Vector3 waitingRotation = new Vector3(21f, 0f, 0f);
+        Vector3 playingRotation = new Vector3(40f, 0f, 0f);
+
+        if (GameManager.State == GameState.Waiting)
+        {
+            lastState = GameState.Waiting;
+
+            Transform.position = waitingPosition;
+            Transform.eulerAngles = waitingRotation;
+            blur.interpolation = 1f;
+        }
+        else
+        {
+            //cache the starting lerp values
+            //when the game goes to starting state
+            if (lastState == GameState.Waiting)
+            {
+                lastState = GameState.Starting;
+
+
+            }
+
+            float t = startingPlayingCurve.Evaluate(GameManager.StartTime / GameManager.StartDuration);
+            Transform.position = Vector3.Lerp(waitingPosition, playingPosition, t);
+
+            Vector3 euler = Transform.eulerAngles;
+            euler.x = Mathf.LerpAngle(waitingRotation.x, playingRotation.x, t);
+            euler.y = Mathf.LerpAngle(waitingRotation.y, playingRotation.y, t);
+            euler.z = Mathf.LerpAngle(waitingRotation.z, playingRotation.z, t);
+            Transform.eulerAngles = euler;
+
+            blur.interpolation = Mathf.Lerp(blur.interpolation, GameManager.Paused ? 1f : 0f, t);
+
+            Transform.position += UnityEngine.Random.onUnitSphere * shake;
+        }
+
+        shake = Mathf.Lerp(shake, 0f, Time.deltaTime * shakeSettle);
+        blur.enabled = blur.interpolation > 0.01f;
+
+        if (GameManager.State == GameState.Starting)
+        {
+            startingTime += Time.fixedDeltaTime;
+        }
+        else
+        {
+            startingTime = 0f;
+        }
+
+        //if celeberating, focus on the winner
+        if (GameManager.State == GameState.Celebrating && GameManager.Winner)
+        {
+            celebratingTime += Time.fixedDeltaTime;
+            float t = celebratingTime / GameManager.CelebrateDuration;
+            cam.fieldOfView = Mathf.Lerp(fov, 5f, focusOnWinner.Evaluate(t));
+            Transform.LookAt(GameManager.Winner.transform.position);
+        }
+        else
+        {
+            celebratingTime = 0f;
+        }
+    }
+
+    public static void Shake()
+    {
+        if (!instance) instance = FindObjectOfType<CameraManager>();
+
+        instance.shake = 1f;
+    }
+}
---
+++ Assets/Game/Code/Player/Player.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Player : MonoBehaviour
+{
+    public PlayerSettings settings;
+
+    public int joyconIndex = 0;
+    public int lives = 0;
+
+    [ColorUsage(false)]
+    public Color playerColor = Color.white;
+
+    public float Steer
+    {
+        get
+        {
+            float steer = 0f;
+            if (JoyconManager.Joycons.Count > joyconIndex)
+            {
+                steer = (90f - JoyconManager.Joycons[joyconIndex].Euler.z) / 180f;
+                float abs = Mathf.Abs(steer);
+                if (abs < 0.04f)
+                {
+                    steer = 0f;
+                }
+                else
+                {
+                    if (abs > 0.15f)
+                    {
+                        steer = Mathf.Sign(steer);
+                    }
+                    else
+                    {
+                        steer = Mathf.Sign(steer) * 1f;
+                    }
+                }
+
+                return steer;
+            }
+
+            steer = 0f;
+            if (Input.GetKey(KeyCode.A)) steer -= 1f;
+            if (Input.GetKey(KeyCode.D)) steer += 1f;
+
+            if (joyconIndex == 1)
+            {
+                steer = 0f;
+                if (Input.GetKey(KeyCode.LeftArrow)) steer -= 1f;
+                if (Input.GetKey(KeyCode.RightArrow)) steer += 1f;
+            }
+
+            return steer;
+        }
+    }
+
+    private Vector3 originalPosition;
+    private Vector3 originalRotation;
+    private PlayerMovement movement;
+    private bool died;
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+        originalPosition = transform.position;
+        originalRotation = transform.eulerAngles;
+
+        Reset();
+    }
+
+    public void Reset()
+    {
+        lives = settings.lives;
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        //dont respawn if celebrating
+        if (GameManager.State == GameState.Celebrating) return;
+
+        //reset position and rotation
+        transform.position = originalPosition;
+        transform.localEulerAngles = originalRotation;
+
+        died = false;
+
+        //reset velocity
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Vector3.zero;
+        rigidbody.velocity = Vector3.zero;
+
+        //dont subtract lives if not in play mode
+        if (GameManager.State != GameState.Playing) return;
+
+        lives--;
+
+        //this player ran out of lives
+        //end the game
+        if (lives == 0)
+        {
+            //find the player with the most lives
+            int minLives = int.MinValue;
+            Player winner = null;
+            Player[] allPlayers = FindObjectsOfType<Player>();
+            for (int i = 0; i < allPlayers.Length; i++)
+            {
+                if (allPlayers[i].lives > minLives)
+                {
+                    minLives = allPlayers[i].lives;
+                    winner = allPlayers[i];
+                }
+            }
+
+            GameManager.Celebrate(winner);
+            return;
+        }
+    }
+
+    public void ShakeSingle()
+    {
+        if (GameManager.State != GameState.Playing) return;
+
+        if (JoyconManager.Joycons.Count > joyconIndex)
+        {
+            var joycon = JoyconManager.Joycons[joyconIndex];
+            joycon.SetRumble(settings.low, settings.high, settings.amp, 300);
+        }
+    }
+
+    public async void Shake()
+    {
+        if (GameManager.State != GameState.Playing) return;
+
+        if (JoyconManager.Joycons.Count > joyconIndex)
+        {
+            var joycon = JoyconManager.Joycons[joyconIndex];
+
+            joycon.SetRumble(settings.low, settings.high, settings.amp, settings.duration);
+
+            await Task.Delay(settings.duration);
+            await Task.Delay(30);
+
+            joycon.SetRumble(settings.low, settings.high, settings.amp, settings.duration);
+
+            await Task.Delay(settings.duration);
+            await Task.Delay(30);
+
+            joycon.SetRumble(settings.low, settings.high, settings.amp, settings.duration);
+        }
+    }
+
+    private void SetColor()
+    {
+        //change color of material without instancing a new one
+
+        MeshRenderer meshRenderer = GetComponentInChildren<MeshRenderer>();
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        meshRenderer.GetPropertyBlock(block);
+        block.SetColor("_Color", playerColor);
+        block.SetColor("_EmissionColor", playerColor * 0.5f);
+        meshRenderer.SetPropertyBlock(block);
+    }
+
+    private void SetRotation()
+    {
+        //force x rotation to be 0
+
+        Vector3 euler = transform.localEulerAngles;
+        euler.z = 0f;
+        transform.localEulerAngles = euler;
+    }
+
+    private void Update()
+    {
+        SetColor();
+
+        if (Application.isPlaying)
+        {
+            if (GameManager.State == GameState.Starting)
+            {
+                transform.position = originalPosition;
+                transform.localEulerAngles = originalRotation;
+            }
+
+            SetRotation();
+        }
+    }
+
+    private async void OnTriggerEnter(Collider other)
+    {
+        if (died) return;
+        if (GameManager.State == GameState.Starting) return;
+
+        //if hit water, wait 1 seconds and then respawn
+        if (other.name == "Water")
+        {
+            died = true;
+
+            //Play water splash if game is being played
+            if (GameManager.State == GameState.Playing)
+            {
+                FMODUnity.RuntimeManager.CreateInstance("event:/Splash").start();
+            }
+
+            Shake();
+            int ms = Mathf.RoundToInt(settings.respawnDuration * 1000f);
+            await Task.Delay(ms);
+
+            //this check happens when exiting to edit mode
+            if (!this) return;
+
+            Spawn();
+        }
+    }
+}
